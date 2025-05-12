f049cf025b11aa60f5842768006aae713dd80915
JoaoVitorUTFPR <149010108+JoaoVitorUTFPR@users.noreply.github.com>
2025-05-11 21:52:26 -0300

    fiat total aluno

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,7 +9,15 @@ function Home() {
         <Sidebar/>
         <div className={styles.content}>
             <h1 className={styles.title}>Bem-vindo à página Home!</h1>
-            <p>Esse é o conteúdo de Homeeeee.</p>
+            <p>Esse é o conteúdo de Home.</p>
+
+            <div className={styles.card}>
+              <span className={styles.icon}>🎓</span>
+              <div>
+                <p className={styles.text}>Alunos</p>
+                <strong className={styles.count}>120</strong>
+              </div>
+            </div>
         </div>
       </div>
     </>
